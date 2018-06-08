bc4d74401a5d0709eb81a7e71f19bef161e82dc9
Ken Bannister <kb2ma@runbox.com>
2018-06-08 01:15:27 -0400

    Update from screenshot to html page.

---
+++ nanocoap-api-doc_files/navtreedata.js
@@ -0,0 +1,57 @@
+var NAVTREE =
+[
+  [ "RIOT Documentation", "index.html", [
+    [ "RIOT in a nutshell                                        ", "index.html#riot-in-a-nutshell", null ],
+    [ "Contribute to RIOT                                        ", "index.html#contribute-to-riot", null ],
+    [ "The quickest start                                        ", "index.html#the-quickest-start", null ],
+    [ "Structure                                                          ", "index.html#structure", null ],
+    [ "Further information                                      ", "index.html#further-information", null ],
+    [ "Creating modules", "creating-modules.html", [
+      [ "The general structure                                  ", "creating-modules.html#the-general-structure", null ],
+      [ "Modules outside of RIOTBASE                      ", "creating-modules.html#modules-outside-of-riotbase", null ],
+      [ "Pseudomodules                                                  ", "creating-modules.html#pseudomodules", null ]
+    ] ],
+    [ "Creating an application", "creating-an-application.html", [
+      [ "The main function                                          ", "creating-an-application.html#the-main-function", null ],
+      [ "The application's Makefile                         ", "creating-an-application.html#the-applications-makefile", [
+        [ "The minimal Makefile                                    ", "creating-an-application.html#the-minimal-makefile", null ],
+        [ "How to handle unsupported boards?                  ", "creating-an-application.html#handle-unsupported-boards", null ],
+        [ "Including modules                                          ", "creating-an-application.html#including-modules", null ]
+      ] ]
+    ] ],
+    [ "Getting started", "getting-started.html", [
+      [ "Downloading RIOT code                                  ", "getting-started.html#downloading-riot-code", null ],
+      [ "Compiling RIOT                                                ", "getting-started.html#compiling-riot", [
+        [ "Setting up a toolchain                                ", "getting-started.html#setting-up-a-toolchain", null ],
+        [ "The build system                                            ", "getting-started.html#the-build-system", null ],
+        [ "Building and executing an example           ", "getting-started.html#building-and-executing-an-example", null ]
+      ] ]
+    ] ],
+    [ "Changelog", "changelog.html", [
+      [ "Release 2018.04", "changelog.html#release-2018-04", null ],
+      [ "Release 2018.01", "changelog.html#release-2018-01", null ],
+      [ "Release 2017.10", "changelog.html#release-2017-10", null ],
+      [ "Release 2017.07", "changelog.html#release-2017-07", null ],
+      [ "Release 2017.04", "changelog.html#release-2017-04", null ],
+      [ "Release 2017.01", "changelog.html#release-2017-01", null ],
+      [ "Release 2016.10", "changelog.html#release-2016-10", null ],
+      [ "Release 2016.07", "changelog.html#release-2016-07", null ],
+      [ "Release 2016.04", "changelog.html#release-2016-04", null ],
+      [ "Release 2015.12", "changelog.html#release-2015-12", null ],
+      [ "Release 2015.09", "changelog.html#release-2015-09", null ],
+      [ "Release 2014.12", "changelog.html#release-2014-12", null ],
+      [ "Release 2014.05", "changelog.html#release-2014-05", null ],
+      [ "Release 2014.01", "changelog.html#release-2014-01", null ],
+      [ "Release 2013.08", "changelog.html#release-2013-08", null ]
+    ] ],
+    [ "Removed Features and Modules", "md__home_kbee_dev_riot_repo_LOSTANDFOUND.html", null ],
+    [ "Todo List", "todo.html", null ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "fu
