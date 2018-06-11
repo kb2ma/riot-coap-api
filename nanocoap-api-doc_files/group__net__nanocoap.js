1b4eed1c5dd4a47b2d7693ade3624d96523916c6
Ken Bannister <kb2ma@runbox.com>
2018-06-11 01:17:47 -0400

    fixup! option numbers reference

---
+++ nanocoap-api-doc_files/group__net__nanocoap.js
@@ -0,0 +1,73 @@
+var group__net__nanocoap =
+[
+    [ "nanocoap.h", "nanocoap_8h.html", null ],
+    [ "coap_hdr_t", "structcoap__hdr__t.html", [
+      [ "code", "structcoap__hdr__t.html#ad48db11a53a6213f5b1533eeeb224ec1", null ],
+      [ "data", "structcoap__hdr__t.html#a36b9fb820a8633a861e25d8e1dc4a3a9", null ],
+      [ "id", "structcoap__hdr__t.html#adbce2dad56a887dae56403714c92314b", null ],
+      [ "ver_t_tkl", "structcoap__hdr__t.html#a7b1cc0722e22d7c646156c730d17b374", null ]
+    ] ],
+    [ "coap_optpos_t", "structcoap__optpos__t.html", [
+      [ "offset", "structcoap__optpos__t.html#a4defca54e66d1a6158d77290e5010c20", null ],
+      [ "opt_num", "structcoap__optpos__t.html#aef81d19738f6cf41851ec8e18f3dd555", null ]
+    ] ],
+    [ "coap_pkt_t", "structcoap__pkt__t.html", [
+      [ "hdr", "structcoap__pkt__t.html#aa1ffc04c491c11cb0972bd95cb3b3bd8", null ],
+      [ "options", "structcoap__pkt__t.html#a54b1774608b1f2cb867e21c81e979907", null ],
+      [ "options_len", "structcoap__pkt__t.html#abb6e6d0d60c2154b84b3a61e0cddab56", null ],
+      [ "payload", "structcoap__pkt__t.html#a296b66b050d0be92094daf2aba8d730a", null ],
+      [ "payload_len", "structcoap__pkt__t.html#a42765c97fe9d014c311b6b733827a17b", null ],
+      [ "token", "structcoap__pkt__t.html#a2e7247a6cb35b2d855f26fc132624d01", null ]
+    ] ],
+    [ "coap_resource_t", "structcoap__resource__t.html", [
+      [ "context", "structcoap__resource__t.html#aef7bce93888bb2ae217cdb5e983622ce", null ],
+      [ "handler", "structcoap__resource__t.html#aeebf86de786a4b4cd41f480891b08cc5", null ],
+      [ "methods", "structcoap__resource__t.html#a3f5d920720042d9c2092a8f18e6179ce", null ],
+      [ "path", "structcoap__resource__t.html#a1d11fb4d19d132be8091d6ba43558b79", null ]
+    ] ],
+    [ "coap_block1_t", "structcoap__block1__t.html", [
+      [ "blknum", "structcoap__block1__t.html#a204506499b7d199da7b9885c73fcf7a1", null ],
+      [ "more", "structcoap__block1__t.html#ae44e66fac497fadaff09b558eb5612d1", null ],
+      [ "offset", "structcoap__block1__t.html#a2bdc3f271f79437e921d828e16ba99e1", null ],
+      [ "szx", "structcoap__block1__t.html#afbaf9367132de3531a051516985a1600", null ]
+    ] ],
+    [ "COAP_ACK_VARIANCE", "group__net__nanocoap.html#ga23d1305fef42dd158e21376cef6e100a", null ],
+    [ "COAP_FORMAT_NONE", "group__net__nanocoap.html#ga0c9cfbe5210cba4e35c9fbad51a88e19", null ],
+    [ "COAP_PORT", "group__net__nanocoap.html#ga01ef987a44794abfdf83fbf3e02e08b6", null ],
+    [ "COAP_WELL_KNOWN_CORE_DEFAULT_HANDLER", "group__net__nanocoap.html#ga65ee47333edb3b0291e2f9059593efdc", null ],
+    [ "coap_handler_t", "group__net__nanocoap.html#ga687f23858a4a5036b60aedee24fb6b90", null ],
+    [ "coap_build_hdr", "group__net__nanocoap.html#gaa9e0691b6d3d3eb63c63fd13c5dd3820", null ],
+    [ "coap_build_reply", "group__net__nanocoap.html#ga38b2f76446070e80183a13e6b33d7cff", null ],
+    [ "coap_clear_observe", "group__net__nanocoap.html#gaf91b1288f2ef716192b5a6698da8b56b", null ],
+    [ "coap_code", "group__net__nanocoap.html#ga07d2978649c0a31f22c51dc91fc06794", null ],
+    [ "coap_get_block1", "group__net__nanocoap.html#gac6a529a906a33a4f983083a9dcc29265", null ],
+    [ "coap_get_blockopt", "group__net__nanocoap.html#ga40828d99503144806a4e1c5ca9023026", null ],
+    [ "coap_get_code", "group__net__nanocoap.html#ga90c2aae94ab827a245956c4aa6aa3bce", null ],
+    [ "coap_get_code_class", "group__net__nanocoap.html#ga19ea0235c2b8c20bd0be3dfaa05e68c4", null ],
+    [ "coap_get_code_detail", "group__net__nanocoap.html#ga00bb45bdda0c564ff8d8de2ea89b6f46", null ],
+    [ "coap_get_code_raw", "group__net__nanocoap.html#ga430bd1c71178bc8d8772a8df961adabf", null ],
+    [ "coap_get_content_type", "group__net__nanocoap.html#gadfbb9dbf3fb3942898f14d8e2a6f724b", null ],
+    [ "coap_get_id", "group__net__nanocoap.html#ga25bda64c6f3939da4b25e96f72dd2adc", null ],
+    [ "coap_get_observe", "group__net__nanocoap.html#ga944d91c1cb601c49463228cb2c88c3b7", null ],
+    [ "coap_get_token_len", "group__net__nanocoap.html#gae209fb09362106d858bb26f7d046217a", null ],
+    [ "coap_get_total_hdr_len", "group__net__nanocoap.html#gaa3c4e99854ca188e58ade771ad409aab", null ],
+    [ "coap_get_type", "group__net__nanocoap.html#gaacd3b3b147bc37f42171c707bc9b0283", null ],
+    [ "coap_get_uri", "group__net__nanocoap.html#gab4f54f7f10da26f3c7377818f5cd8f43", null ],
+    [ "coap_get_ver", "group__net__nanocoap.html#ga4def81c786e46ce4ba840bc8480e173a", null ],
+    [ "coap_handle_req", "group__net__nanocoap.html#ga04d7380bcf4cdcf583254471a2996470", null ],
+    [ "coap_has_observe", "group__net__nanocoap.html#gaef2702953e01f46c75bea9ad810a4d1b", null ],
+    [ "coap_hdr_set_code", "group__net__nanocoap.html#ga4599c179d69f1df78219c8adbb2ed2ad", null ],
+    [ "coap_hdr_set_type", "group__net__nanocoap.html#ga92058243dac042a617701aaedc58e45d", null ],
+    [ "coap_method2flag", "group__net__nanocoap.html#ga79ad820ea92b0b2ee2954b7aee7139a1", null ],
+    [ "coap_parse", "group__net__nanocoap.html#gad60d02b4df6c510da7931ac15e430378", null ],
+    [ "coap_put_block1_ok", "group__net__nanocoap.html#gaf067e5f033cfd6828b5c9fba2998e96c", null ],
+    [ "coap_put_option", "group__net__nanocoap.html#ga065e34674b2481e0b509b8ff189064d7", null ],
+    [ "coap_put_option_block1", "group__net__nanocoap.html#ga5d6a47efd5a42f1d870bec09439c8905", null ],
+    [ "coap_put_option_ct", "group__net__nanocoap.html#gad05840eede48e0c6c58ba8aac99232ef", null ],
+    [ "coap_put_option_uri", "group__net__nanocoap.html#gac9ada1b09c3143464f4dcb362bcd576c", null ],
+    [ "coap_reply_simple", "group__net__nanocoap.html#ga8c3dc9ffebf2dd089a1d742499aca152", null ],
+    [ "coap_szx2size", "group__net__nanocoap.html#ga63d62de3e0b1e4c9059bfbb5caf7de3b", null ],
+    [ "coap_well_known_core_default_handler", "group__net__nanocoap.html#ga668e87d5f9984d69aee2e71800d8bf65", null ],
+    [ "coap_resources", "group__net__nanocoap.html#ga45419c6b81b39bdd462f0811a597e8ff", null ],
+    [ "coap_resources_numof", "group__net__nanocoap.html#ga5e0d38caa4bafa78426fd03b28a07165", null ]
+];
